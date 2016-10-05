d8abd25add202f0ec4a7696fc9c4704bd3ad3b32
Huan Shao <huanshaoself@gmail.com>
2016-10-05 10:16:24 +0800

    Add Row Select Event And Class

--- assets/js/recharge.js
+++ assets/js/recharge.js
@@ -1,17 +1,27 @@
 /**
  * Created by huan.shao on 10/4/16.
  */
-$('.tabgroup > div').hide();
-$('.tabgroup > div:first-of-type').show();
-$('.tabs a').click(function(e){
-    e.preventDefault();
-    var $this = $(this),
-        tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
-        others = $this.closest('li').siblings().children('a'),
-        target = $this.attr('href');
-    others.removeClass('active');
-    $this.addClass('active');
-    $(tabgroup).children('div').hide();
-    $(target).show();
+jQuery(document).ready(function() {
+    $('.tabgroup > div').hide();
+    $('.tabgroup > div:first-of-type').show();
+    $('.tabs a').click(function(e){
+        e.preventDefault();
+        var $this = $(this),
+            tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
+            others = $this.closest('li').siblings().children('a'),
+            target = $this.attr('href');
+        others.removeClass('active');
+        $this.addClass('active');
+        $(tabgroup).children('div').hide();
+        $(target).show();
 
-})
+    })
+
+
+    $("#table_recharge tr").click(function(){
+        var selected = $(this).hasClass("highlight");
+        $("#table_recharge tr").removeClass("highlight");
+        if(!selected)
+            $(this).addClass("highlight");
+    });
+});
