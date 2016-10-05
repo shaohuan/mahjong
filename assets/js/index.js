fba96bfe0e2e496ceddf0d7a541b75799827922f
Huan Shao <huanshaoself@gmail.com>
2016-10-05 14:51:03 +0800

    Dynamically Create Table

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,45 +1,38 @@
 
 jQuery(document).ready(function() {
     $('.page-container form').submit(function(){
-
-        // $.ajax({
-        //     url: 'http://api.joind.in/v2.1/talks/10889',
-        //     data: {
-        //         format: 'json'
-        //     },
-        //     error: function() {
-        //         $('#info').html('<p>An error has occurred</p>');
-        //     },
-        //     success: function(data) {
-        //         localStorage.token = "loginIn";
-        //         window.location.href='recharge.html';
-        //     },
-        //     type: 'POST'
-        // });
-
-
         var username = $(this).find('.username').val();
         var password = $(this).find('.password').val();
         if(username == '') {
-            $(this).find('.error').fadeIn('fast', function(){
-                $(this).find('span').html("用户名不能为空");
-                $(this).parent().find('.username').focus();
-            });
-
-            $(this).find('.error').fadeOut(2000);
-            return false;
+            showError("用户名不能为空");
+            return;
         }
         if(password == '') {
-
-            $(this).find('.error').fadeIn('slow', function(){
-                $(this).find('span').html("密码不能为空");
-                $(this).parent().find('.password').focus();
-
-            });
-            $(this).find('.error').fadeOut('slow', function(){
-            });
-            return false;
+            showError("密码不能为空");
         }
+
+        $.post('/v0/account/login', { user_name: username, field2 : password},
+            function(returnedData){
+                alert('success');
+                localStorage.user = {'user_id':username,token:"xxx"};
+                console.log(returnedData);
+            }).fail(function(){
+                console.log("error");
+                localStorage.user = {'user_id':username,token:"xxx"};
+        });
     });
 
+
+
+
+
+
+    function showError(text){
+        $(this).find('.error').fadeIn('fast', function(){
+            $(this).find('span').html();
+            $(this).parent().find('.username').focus();
+        });
+
+        $(this).find('.error').fadeOut(2000);
+    }
 });
--- assets/js/recharge.js
+++ assets/js/recharge.js
@@ -18,25 +18,93 @@ jQuery(document).ready(function() {
     })
 
 
-    $("#table_recharge tr").click(function(){
+
+    $("#table_users").delegate("tr", "click", function(){
         //获取ID值
         var value=$(this).closest('tr').children('td:first').text();
 
         var selected = $(this).hasClass("highlight");
-        $("#table_recharge tr").removeClass("highlight");
-        if(!selected)
+        $("#table_users tr").removeClass("highlight");
+        if(!selected){
             $(this).addClass("highlight");
+        }
     });
     
     
     $("#tab2 div.record_query_btn").click(function () {
-        alert('请求AJAX');
+       var res = {
+            "error_code":0,
+            "msg":"success",
+            "user_list":[
+                    {
+                    "user_id":123456,
+                    "user_name":"张三",
+                    "headimgurl":"http://xxxx",
+                    "sex":1,
+                    "doudou":10,
+                    "total_rounds":10,
+                    "win_rounds":3,
+                    "total_score":0
+                },
+                {
+                    "user_id":123456,
+                    "user_name":"张三",
+                    "headimgurl":"http://xxxx",
+                    "sex":1,
+                    "doudou":10,
+                    "total_rounds":10,
+                    "win_rounds":3,
+                    "total_score":0
+                },
+                {
+                    "user_id":123456,
+                    "user_name":"张三",
+                    "headimgurl":"http://xxxx",
+                    "sex":1,
+                    "doudou":10,
+                    "total_rounds":10,
+                    "win_rounds":3,
+                    "total_score":0
+                }
+             ]
+        };
+
+
+        //渲染table
+        loadData2UserTable("table_users",res.user_list)
     });
-
-
 
 });
 
+
+function include(arr, obj) {
+    for(var i=0; i<arr.length; i++) {
+        if (arr[i] == obj) return true;
+    }
+}
+
+function loadData2UserTable(tableId,data) {
+    var displayCols = ['user_id','user_name','headimgurl'];
+    var r = new Array(), j = -1;
+    for (var i=0, size=data.length; i<size; i++){
+        r[++j] ='<tr>';
+        $.each(data[i], function(key, value){
+            if (key == "headimgurl"){
+                r[++j] ='<td data-table-header="' +key+ '">';
+                r[++j] = '<img alt="头像" src="' + value + ' ></td>';
+                return true;
+            }
+
+            if (include(displayCols,key)){
+                r[++j] ='<td data-table-header="' +key+ '">';
+                r[++j] = value + '</td>';
+            }
+        });
+        r[++j] ='<td data-table-header="...">...</td>';
+        r[++j] = '</tr>';
+    }
+    $('#' +tableId + " tbody").html(r.join(''));
+}
 
 
 function fetchData(url,type,params,onSuccess,onError){
