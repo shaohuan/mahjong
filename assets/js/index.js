fbbb9b4475ce99f0c75d47297c4a3a695b329e9c
Huan Shao <huanshaoself@gmail.com>
2016-10-05 21:05:37 +0800

    Remove test code

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,7 +13,31 @@ jQuery(document).ready(function() {
             return false;
         }
 
-        $.post('/v0/account/login', {user_name: username, field2: password},
+        function logResults(json){
+            console.log(json);
+        }
+
+
+        // $.ajax({
+        //     url: "http://114.55.148.214:8000/login",
+        //
+        //     jsonp: "callback",
+        //
+        //     dataType: "jsonp",
+        //
+        //     data: {
+        //         user_name:username,
+        //         password: password
+        //     },
+        //
+        //     success: function( response ) {
+        //         console.log( response ); // server response
+        //     }
+        // });
+        //
+        // return;
+
+        $.post('/v0/account/login', {user_name: username, password: password},
             function (res) {
                 var res = {
                     "error_code":0,
@@ -25,19 +49,38 @@ jQuery(document).ready(function() {
                     "sex":1,
                     "doudou":10,
                     "total_rounds":10,
+                    "role_name":"admin",
                     "win_rounds":3,
                     "total_score":0
                 };
 
-                localStorage.user_id = res.user_id;
-                localStorage.token = res.token;
-                localStorage.headimgurl = res.headimgurl;
-                localStorage.user_name = res.user_name;
-                
+                if (res.error_code != 0){
+                    showLoginErr("登陆失败!" + "  状态码："+res.error_code);
+                    return;
+                }
+
+
+                updateLocalStorage(res);
                 window.location.href = "recharge.html";
             }).fail(function (response) {
                 showLoginErr("登陆失败!" + "  状态码："+response.status);
-                window.location.href = "recharge.html";
+
+
+            //测试代码
+            updateLocalStorage({
+                "error_code":0,
+                "token":"abdgfsjfds",
+                "msg":"success",
+                "user_id":123456,
+                "user_name":"张三",
+                "headimgurl":"http://xxxx",
+                "sex":1,
+                "doudou":10,
+                "total_rounds":10,
+                "role_name":"agent",
+                "win_rounds":3,
+                "total_score":0
+            });
         });
     });
     function showLoginErr(msg) {
@@ -46,6 +89,16 @@ jQuery(document).ready(function() {
 
     function hideLoginErr() {
         $('#login-error').css('visibility', 'hidden');
+    }
+
+    //更新localStorage
+    function updateLocalStorage(res){
+        localStorage.user_id = res.user_id;
+        localStorage.token = res.token;
+        localStorage.headimgurl = res.headimgurl;
+        localStorage.user_name = res.user_name;
+        localStorage.doudou = res.doudou;
+        localStorage.role_name = res.role_name;
     }
 });
 
--- assets/js/recharge.js
+++ assets/js/recharge.js
@@ -2,6 +2,15 @@
  * Created by huan.shao on 10/4/16.
  */
 jQuery(document).ready(function() {
+
+
+    function  updateUserProfile(){
+        $('#user_name').html(localStorage.getItem("user_name"));
+        $('#role_name').html(localStorage.getItem("role_name")=="admin"?"超级用户":"代理用户");
+        $('#doudou').html(localStorage.getItem("doudou"));
+    };
+    updateUserProfile();
+
     $('.tabgroup > div').hide();
     $('.tabgroup > div:first-of-type').show();
     $('.tabs a').click(function(e){
@@ -15,12 +24,18 @@ jQuery(document).ready(function() {
         $(tabgroup).children('div').hide();
         $(target).show();
 
-    })
+    });
+
+
+    if(!!localStorage.role_name && localStorage.role_name=="agent"){
+        $('#query_type').css('visibility','visible');
+    }else{
+        $('#query_type').css('visibility','hidden');
+    };
 
 
 
     //选择用户的ID
-
     $("#table_users").delegate("tr", "click", function(){
         // var value = $(this).closest('tr').children('td:first').text();
         var selected = $(this).hasClass("highlight");
@@ -31,204 +46,147 @@ jQuery(document).ready(function() {
     });
 
 
-    function showRechargeErr(msg){
-        $('#recharge-error').html(msg).css('visibility','visible');
+    function showErr(msg,id){
+        $('#'+id).html(msg).css('visibility','visible');
     }
-    
-    function hideRechargeErr() {
-        $('#recharge-error').css('visibility','hidden');
-        
+
+    function hideErr(id) {
+        $('#'+id).css('visibility','hidden');
     }
-    
+
     //确认充值
     $('#confirm_recharge').click(function () {
         var beans = $('#beans').val();
         var selectedRow = $("#table_users tr.highlight");
         if (selectedRow.length == 0){
-            showRechargeErr("请先选择一个用户");
+            showErr("请先选择一个用户",'recharge-error');
             return;
         }
 
         if (isNaN(beans) || beans.trim().length==0){
-            showRechargeErr("柑桔必须为数字");
+            showErr("柑桔必须为数字","recharge-error");
             return;
         }
 
         var user_id = selectedRow.children('td:first').text();
-        fetchData('/v0/order/create','POST',{'user_id':user_id,'beans':beans},function (res) {
-            hideRechargeErr();
+        fetchData('/v0/order/create','POST',{'user_id':user_id,'beans':parseInt(beans)},function (res) {
+            hideErr();
         },function (msg) {
-            showRechargeErr(msg);
-
-        });
+            showErr(msg,'recharge-error');
+        },'recharge-error');
     });
     $('#search_user_btn').on('click',function () {
         //查询用户
-        var res = {
-            "error_code":0,
-            "msg":"success",
-            "user_list":[
-                {
-                    "user_id":123456,
-                    "user_name":"张三",
-                    "headimgurl":"http://xxxx",
-                    "sex":1,
-                    "doudou":10,
-                    "total_rounds":10,
-                    "win_rounds":3,
-                    "total_score":0
-                },
-                {
-                    "user_id":123456,
-                    "user_name":"张三",
-                    "headimgurl":"http://xxxx",
-                    "sex":1,
-                    "doudou":10,
-                    "total_rounds":10,
-                    "win_rounds":3,
-                    "total_score":0
-                },
-                {
-                    "user_id":123456,
-                    "user_name":"张三",
-                    "headimgurl":"http://xxxx",
-                    "sex":1,
-                    "doudou":10,
-                    "total_rounds":10,
-                    "win_rounds":3,
-                    "total_score":0
-                }
-            ]
+
+        var role_name = localStorage.getItem('role_name');
+        var params = {},searchKey = $('#user_key').val();
+        if (searchKey.trim().length == 0){
+            showErr("查询关键字不能为空",'recharge-error')
+            return;
+        }
+
+        if (role_name == 'agent'){
+            var userType = $("#query_type").val();
+            params[userType] = searchKey;
         };
 
-        loadData2UserTable("table_users",res.user_list);
-        $("#recharge_amount").css('visibility','visible');
 
-    })
-    
-    
+        fetchData(' /v0/account/query_list','GET',params,function (res) {
+            loadData2UserTable("table_users",res.user_list);
+            $("#recharge_amount").css('visibility','visible');
+            hideErr();
+
+        },function (msg) {
+            showErr(msg,'recharge-error');
+        },'recharge-error');
+    });
+
+
+    //查询充值历史记录
     $("#tab2 div.record_query_btn").click(function () {
-        var res =         {
-            "error_code":0,
-            "msg":"success",
-            "order_list":[
-                {
-                    "orderid":"wx0001",
-                    "uid":222,
-                    "beans":0,
-                    "fee":0,
-                    "status":0,
-                    "descrption":"xxxx",
-                    "creation_time":132
-                },
-                {
-                    "orderid":"wx0001",
-                    "uid":222,
-                    "beans":0,
-                    "fee":0,
-                    "status":0,
-                    "descrption":"xxxx",
-                    "creation_time":132
-                },
-                {
-                    "orderid":"wx0001",
-                    "uid":222,
-                    "beans":0,
-                    "fee":0,
-                    "status":0,
-                    "descrption":"xxxx",
-                    "creation_time":132
-                },
-                {
-                    "orderid":"wx0001",
-                    "uid":222,
-                    "beans":0,
-                    "fee":0,
-                    "status":0,
-                    "descrption":"xxxx",
-                    "creation_time":132
-                }
-            ]
-        };
-
-
-
-        //渲染table
-        loadData2HistoryTable("order_list",res.order_list);
-
+        fetchData(' /v0/order/query','GET',{},function (res) {
+            //渲染table
+            loadData2HistoryTable("order_list",res.order_list);
+            $("#recharge_amount").css('visibility','visible');
+            hideErr();
+        },function (msg) {
+            showErr(msg,'recharge-error');
+        },'history-error');
 
     });
+
+    //检查数组中是否包含某个元素
+    function include(arr, obj) {
+        for(var i=0; i<arr.length; i++) {
+            if (arr[i] == obj) return true;
+        }
+    }
+
+    function loadData2HistoryTable(tableId,data) {
+
+        var dict = {"orderid":"充值ID","uid":"用户ID","nick_name":"昵称","creation_time":"时间","fee":"充值数","beans":"余额","descrption":"描述",
+            "status":"订单状态"};
+        var r = new Array(), j = -1, desc="";
+        for (var i=0, size=data.length; i<size; i++){
+            r[++j] ='<tr>';
+            $.each(data[i], function(key, value){
+                desc = !!dict[key]?dict[key]:"";
+                r[++j] ='<td data-table-header="' +desc+ '">';
+                r[++j] = value + '</td>';
+            });
+            r[++j] = '</tr>';
+        }
+        $('#' +tableId + " tbody").html(r.join(''));
+    }
+
+
+    function loadData2UserTable(tableId,data) {
+        var displayCols = ['user_id','user_name','headimgurl'];
+        var r = new Array(), j = -1;
+        for (var i=0, size=data.length; i<size; i++){
+            r[++j] ='<tr>';
+            $.each(data[i], function(key, value){
+                if (key == "headimgurl"){
+                    r[++j] ='<td data-table-header="' +key+ '">';
+                    r[++j] = '<img alt="头像" src="' + value + ' ></td>';
+                    return true;
+                }
+
+                if (include(displayCols,key)){
+                    r[++j] ='<td data-table-header="' +key+ '">';
+                    r[++j] = value + '</td>';
+                }
+            });
+            r[++j] ='<td data-table-header="...">...</td>';
+            r[++j] = '</tr>';
+        }
+        $('#' +tableId + " tbody").html(r.join(''));
+    }
+
+
+    function fetchData(url,type,params,onSuccess,onError,error_src){
+        $.ajax({
+            url: url,
+            contentType: 'application/json; charset=utf-8',
+            data:$.extend(params,{token:localStorage.token}),
+            error: function(response) {
+                onError();
+                if (response.status != 200){
+                    $('#'+error_src).html("请求出错，错误代码："+response.status).css('visibility','visible');
+                    return;
+                }
+                if(!!response['error_code'] && response['error_code']==10041){
+                    alert("Token过期，请重新登陆!")
+                    localStorage.removeItem('token');
+                    window.location.href='index.html';
+                }
+
+            },
+
+            success: onSuccess,
+            type: type
+        });
+    }
 
 });
 
-//检查数组中是否包含某个元素
-function include(arr, obj) {
-    for(var i=0; i<arr.length; i++) {
-        if (arr[i] == obj) return true;
-    }
-}
-
-function loadData2HistoryTable(tableId,data) {
-
-    var dict = {"orderid":"充值ID","uid":"用户ID","nick_name":"昵称","creation_time":"时间","fee":"充值数","beans":"余额","descrption":"描述",
-                    "status":"订单状态"};
-    var r = new Array(), j = -1, desc="";
-    for (var i=0, size=data.length; i<size; i++){
-        r[++j] ='<tr>';
-        $.each(data[i], function(key, value){
-            desc = !!dict[key]?dict[key]:"";
-            r[++j] ='<td data-table-header="' +desc+ '">';
-            r[++j] = value + '</td>';
-        });
-        r[++j] = '</tr>';
-    }
-    $('#' +tableId + " tbody").html(r.join(''));
-}
-
-
-function loadData2UserTable(tableId,data) {
-    var displayCols = ['user_id','user_name','headimgurl'];
-    var r = new Array(), j = -1;
-    for (var i=0, size=data.length; i<size; i++){
-        r[++j] ='<tr>';
-        $.each(data[i], function(key, value){
-            if (key == "headimgurl"){
-                r[++j] ='<td data-table-header="' +key+ '">';
-                r[++j] = '<img alt="头像" src="' + value + ' ></td>';
-                return true;
-            }
-
-            if (include(displayCols,key)){
-                r[++j] ='<td data-table-header="' +key+ '">';
-                r[++j] = value + '</td>';
-            }
-        });
-        r[++j] ='<td data-table-header="...">...</td>';
-        r[++j] = '</tr>';
-    }
-    $('#' +tableId + " tbody").html(r.join(''));
-}
-
-
-function fetchData(url,type,params,onSuccess,onError){
-    $.ajax({
-        url: url,
-        contentType: 'application/json; charset=utf-8',
-        data:$.extend(params,{token:localStorage.token}),
-        error: function(response) {
-            if (response.status != 200){
-                $('#recharge-error').html("请求出错，错误代码："+response.status).css('visibility','visible');
-                return;
-            }
-            if(!!response['error_code'] && response['error_code']==10041){
-                alert("Token过期，请重新登陆!")
-                localStorage.removeItem('token');
-                window.location.href='index.html';
-            }
-
-        },
-
-        success: onSuccess,
-        type: type
-    });
-}
