5771b69c46a51cd4fc7225e0ba4d9bf7bf75a00d
Huan Shao <huanshaoself@gmail.com>
2016-10-05 18:32:53 +0800

    Login Login Finished

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,47 +1,52 @@
 
 jQuery(document).ready(function() {
-    $('.page-container form').submit(function(){
+    $('.page-container form').submit(function (e) {
+        event.preventDefault();
         var username = $(this).find('.username').val();
         var password = $(this).find('.password').val();
-        if(username == '') {
-            showError("用户名不能为空");
-            return;
+        if (username == '') {
+            showLoginErr("用户名不能为空");
+            return false;
         }
-        if(password == '') {
-            showError("密码不能为空");
+        if (password == '') {
+            showLoginErr("密码不能为空");
+            return false;
         }
 
-        // $.post('/v0/account/login', { user_name: username, field2 : password},
-        //     function(returnedData){
-        //         localStorage.user = {'user_id':username,token:"xxx"};
-        //         console.log(returnedData);
-        //     }).fail(function(){
-        //         console.log("error");
-        //         localStorage.user = {'user_id':username,token:"xxx"};
-        // });
+        $.post('/v0/account/login', {user_name: username, field2: password},
+            function (res) {
+                var res = {
+                    "error_code":0,
+                    "token":"abdgfsjfds",
+                    "msg":"success",
+                    "user_id":123456,
+                    "user_name":"张三",
+                    "headimgurl":"http://xxxx",
+                    "sex":1,
+                    "doudou":10,
+                    "total_rounds":10,
+                    "win_rounds":3,
+                    "total_score":0
+                };
 
-        // $.ajax({
-        //     url: 'some-url',
-        //     type: 'post',
-        //     dataType: 'json',
-        //     data: $('form#myForm').serialize(),
-        //     success: function(data) {
-        //         ... do something with the data...
-        //     }
-        // });
-    });
-
-
-
-
-
-
-    function showError(text){
-        $(this).find('.error').fadeIn('fast', function(){
-            $(this).find('span').html();
-            $(this).parent().find('.username').focus();
+                localStorage.user_id = res.user_id;
+                localStorage.token = res.token;
+                localStorage.headimgurl = res.headimgurl;
+                localStorage.user_name = res.user_name;
+                
+                window.location.href = "recharge.html";
+            }).fail(function (response) {
+                showLoginErr("登陆失败!" + "  状态码："+response.status);
+                window.location.href = "recharge.html";
         });
+    });
+    function showLoginErr(msg) {
+        $('#login-error').html(msg).css('visibility', 'visible');
+    }
 
-        $(this).find('.error').fadeOut(2000);
+    function hideLoginErr() {
+        $('#login-error').css('visibility', 'hidden');
     }
 });
+
+
